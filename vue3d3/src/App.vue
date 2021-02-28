1189f22c1d995dbdabd7a0d2498c6aead4d41afd
ben <ben@nexuminc.com>
2021-02-28 12:40:24 -0600

    Beginning of d3

---
+++ vue3d3/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import GraphComponent from './components/GraphComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GraphComponent
+  }
+}
+</script>
+<template>
+  <GraphComponent title="Simple Graph"/>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3d3/src/components/GraphComponent.vue
@@ -0,0 +1,40 @@
+<script>
+  const d3 = require('d3');
+  export default {
+    name: 'GraphComponent',
+    props: ['title'],
+    methods: {
+      generate: function (){
+
+      }
+    },
+    mounted () {
+      let svg = d3.select("#graphItem").append('svg')
+          .attr("width",500)
+          .attr("height",300)
+      let scale = d3.scaleLinear()
+          .domain([-100,100])
+          .range([0,400])
+      let yScale = d3.scaleLinear()
+          .domain([0,400])
+          .range([-100,100])
+      let xAxis = d3.axisBottom().scale(scale);
+      let yAxis = d3.axisRight().scale(yScale);
+      svg.append('g').attr("transform","translate(20,282)").call(xAxis).append('g').attr("transform","translation(50,10)").call(yAxis);
+      return true;
+    }
+  }
+</script>
+
+<template>
+  <div id="graphComponent">
+    <h1 @click="this.generate()">{{title}}</h1>
+    <div id ="graphItem">
+      Graph goes here.
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
